c4dc675744ccceda8156d6042c4531bbccc771a8
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-09-12 18:40:52 +0530

    adding title to Toolbar and text color as white

--- XYZReader/src/main/java/com/example/xyzreader/ui/detail/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/detail/ArticleDetailFragment.java
@@ -1,22 +1,34 @@
 package com.example.xyzreader.ui.detail;
 
 
-import android.app.LoaderManager;
-import android.content.Loader;
+
 import android.database.Cursor;
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
 import com.example.xyzreader.data.ItemsContract;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ArticleDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 	private long articleId;
-
+	@BindView(R.id.img_article_detail)ImageView imgArticlePhoto;
+	@BindView(R.id.txt_article_title)TextView tvArticleTitle;
+	@BindView(R.id.txt_article_body)TextView tvArticleBody;
+	@BindView(R.id.toolbar_article_detail)Toolbar toolbar;
 	public ArticleDetailFragment() {
 	}
 
@@ -26,7 +38,8 @@ public class ArticleDetailFragment extends Fragment implements LoaderManager.Loa
 		// Inflate the layout for this fragment
 		View rootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
 		articleId = ItemsContract.Items.getItemId(getActivity().getIntent().getData());
-
+		getLoaderManager().initLoader(0,null,this);
+		ButterKnife.bind(this,rootView);
 		return rootView;
 	}
 
@@ -39,8 +52,20 @@ public class ArticleDetailFragment extends Fragment implements LoaderManager.Loa
 	}
 
 	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		data.moveToFirst();
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+		if (cursor==null || !cursor.moveToFirst())
+			return;
+
+		String photoUrl = cursor.getString(ArticleLoader.Query.PHOTO_URL);
+		Glide.with(this).load(photoUrl).into(imgArticlePhoto);
+		String val_article_title = cursor.getString(ArticleLoader.Query.TITLE);
+		String val_article_date = cursor.getString(ArticleLoader.Query.PUBLISHED_DATE);
+		String val_article_author = cursor.getString(ArticleLoader.Query.AUTHOR);
+		String val_article_body = cursor.getString(ArticleLoader.Query.BODY);
+
+//		tvArticleTitle.setText(val_article_title);
+		tvArticleBody.setText(val_article_body);
+		toolbar.setTitle(val_article_title);
 	}
 
 	@Override
