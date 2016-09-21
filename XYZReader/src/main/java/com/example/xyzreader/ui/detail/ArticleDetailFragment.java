360c4c4243235801fc63d4df4e18a18c4ad5e8fb
Rohit Karadkar <karadkar.rohit@gmail.com>
2016-09-21 19:34:23 +0530

    moving article detail title to content body from CollapsingToolbar & adding palette color to their background

--- XYZReader/src/main/java/com/example/xyzreader/ui/detail/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/detail/ArticleDetailFragment.java
@@ -3,22 +3,30 @@ package com.example.xyzreader.ui.detail;
 
 
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
 import com.example.xyzreader.data.ItemsContract;
@@ -27,19 +35,23 @@ import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 
-public class ArticleDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
+public class ArticleDetailFragment extends Fragment implements
+		LoaderManager.LoaderCallbacks<Cursor>,RequestListener<String,GlideDrawable>{
 	private long articleId;
 	@BindView(R.id.iv_article_detail_image)ImageView imgArticlePhoto;
 	@BindView(R.id.tv_article_body)TextView tvArticleBody;
 	@BindView(R.id.toolbar_article_detail)Toolbar toolbar;
 	@BindView(R.id.tv_article_info)TextView tvArticleInfo;
-	@BindView(R.id.collapsing_toolbar_article_detail)CollapsingToolbarLayout collapsingToolbarLayout;
-	ArticleDetailFragment fragment = this;
+	@BindView(R.id.tv_article_title)TextView tvArticleTitle;
+//	@BindViews({R.id.tv_article_title,R.id.tv_article_info}) List<TextView> articleHeaders;
+	@BindView(R.id.article_detail_body_headers)LinearLayout articleHeaders;
 
 	public ArticleDetailFragment() {
 	}
@@ -77,7 +89,10 @@ public class ArticleDetailFragment extends Fragment implements LoaderManager.Loa
 			return;
 
 		String photoUrl = cursor.getString(ArticleLoader.Query.PHOTO_URL);
-		Glide.with(this).load(photoUrl).into(imgArticlePhoto);
+		Glide.with(this)
+			.load(photoUrl)
+			.listener(this)
+			.into(imgArticlePhoto);
 		
 		String val_article_title = cursor.getString(ArticleLoader.Query.TITLE);
 		long val_article_date = cursor.getLong(ArticleLoader.Query.PUBLISHED_DATE);
@@ -90,12 +105,29 @@ public class ArticleDetailFragment extends Fragment implements LoaderManager.Loa
 						" by "+
 						val_article_author
 		);
-//		collapsingToolbarLayout.setTitle(val_article_title);
-		toolbar.setTitle(val_article_title);
+//		toolbar.setTitle(val_article_title);
+		tvArticleTitle.setText(val_article_title);
 	}
 
 	@Override
 	public void onLoaderReset(Loader<Cursor> loader) {
 
+	}
+
+	@Override
+	public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+		return false;
+	}
+
+	@Override
+	public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target,
+	                               boolean isFromMemoryCache, boolean isFirstResource) {
+		Bitmap bitmap = ((GlideBitmapDrawable)resource.getCurrent()).getBitmap();
+		Palette palette = Palette.from(bitmap).generate();
+		int defaultDarkColor = ContextCompat.getColor(getContext(),R.color.cardview_dark_background);
+
+		int darkColor = palette.getDarkMutedColor(defaultDarkColor);
+		articleHeaders.setBackgroundColor(darkColor);
+		return false;
 	}
 }
